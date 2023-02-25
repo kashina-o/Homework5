de9af56502edceda17e2ccca5b987306bd90e802
Olga <poteshkina-o99@mail.ru>
2023-02-25 22:00:13 +0500

    add Task 38

---
+++ Task38/Task38.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] CreateArrayOfRealNumbers(int size)
+ {
+    return new double [size];
+ }
+
+void FillArrayOfRealNumbers(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+            Console.WriteLine(array[i] + " ");
+        }
+}
+
+void DifOfMaxMin(double[] numbers)
+{
+double min = numbers.Min();
+double max = numbers.Max();
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {max-min}");  
+}
+
+int size = Input("Введите длину массива: ");
+double[] array = CreateArrayOfRealNumbers(size);
+FillArrayOfRealNumbers(array);
+DifOfMaxMin(array);
